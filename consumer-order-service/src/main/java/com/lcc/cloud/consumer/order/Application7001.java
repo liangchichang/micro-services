fb0be015dfa6090a2205fad6a69e56dfcbdefa7b
lcc <liangchichang@foxmail.com>
2020-08-30 05:26:55 +0800

    引入单机eureka

--- consumer-order-service/src/main/java/com/lcc/cloud/consumer/order/Application7001.java
+++ consumer-order-service/src/main/java/com/lcc/cloud/consumer/order/Application7001.java
@@ -2,15 +2,17 @@ package com.lcc.cloud.consumer.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lcc
  * @version 2020/8/23
  */
 @SpringBootApplication
-public class Application {
+@EnableEurekaClient
+public class Application7001 {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(Application7001.class, args);
   }
 }
--- payments-service-8001/src/main/java/com/lcc/cloud/payment/Application8001.java
+++ payments-service-8001/src/main/java/com/lcc/cloud/payment/Application8001.java
@@ -2,15 +2,17 @@ package com.lcc.cloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lcc
  * @version 2020/8/21
  */
 @SpringBootApplication
-public class Application {
+@EnableEurekaClient
+public class Application8001 {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(Application8001.class, args);
   }
 }
